d9951aa15468481e7eb5e75db9cadcf4da38d52d
Linda <stayathomegeek@gmail.com>
2020-02-16 18:08:53 -0500

    replace the deprecated staticfiles in base.html with static

--- solosandbox/settings.py
+++ solosandbox/settings.py
@@ -145,7 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
+
+STATICFILES_DIRS = (   # tells your devserver where the static files are
     os.path.join(BASE_DIR, 'static'),  # Linda added this
 )
 
